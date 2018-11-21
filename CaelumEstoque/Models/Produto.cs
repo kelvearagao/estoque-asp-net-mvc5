9ab2dd89afe996d408936ec973b487b7d998b50f
Francisco Kelve Aragao - Ifactory <ifactory.francisco@easynvest.com.br>
2018-11-21 18:10:29 -0200

    Adiciona validacao range e required

--- CaelumEstoque/Models/Produto.cs
+++ CaelumEstoque/Models/Produto.cs
@@ -13,12 +13,14 @@ namespace CaelumEstoque.Models
         [StringLength(20)]
         public String Nome { get; set; }
 
+        [Range(0.0, 10000.0)]
         public float Preco { get; set; }
 
         public CategoriaDoProduto Categoria { get; set; }
 
         public int? CategoriaId { get; set; }
 
+        [Required]
         public String Descricao { get; set; }
 
         public int Quantidade { get; set; }
