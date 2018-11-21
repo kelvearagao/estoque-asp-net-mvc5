b5ac1922b4d2161254d4351b7130fa75b915bf49
Francisco Kelve Aragao - Ifactory <ifactory.francisco@easynvest.com.br>
2018-11-21 18:34:37 -0200

    Adiciona página pra visualizar produto

--- CaelumEstoque/Controllers/ProdutoController.cs
+++ CaelumEstoque/Controllers/ProdutoController.cs
@@ -16,9 +16,7 @@ namespace CaelumEstoque.Controllers
             ProdutosDAO dao = new ProdutosDAO();
             IList<Produto> produtos = dao.Lista();
 
-            ViewBag.Produtos = produtos;
-
-            return View();
+            return View(produtos);
         }
 
         public ActionResult Form()
@@ -56,6 +54,15 @@ namespace CaelumEstoque.Controllers
 
                 return View("Form");
             }
+        }
+
+        public ActionResult Visualiza(int id)
+        {
+            ProdutosDAO dao = new ProdutosDAO();
+            Produto produto = dao.BuscaPorId(id);
+            ViewBag.Produto = produto;
+
+            return View(produto);
         }
     }
 }
